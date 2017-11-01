788880fcdc1cebac679111b9b011cfcb538aae8e
Sebastian Jensen <apps@tsjdev-apps.de>
2017-11-01 14:20:48 +0100

    - add services

---
+++ QuoteCortanaSkill/QuoteCortanaSkill/Services/HttpService.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace QuoteCortanaSkill.Services
+{
+    public static class HttpService
+    {
+        public static async Task<TClass> GetRequestAsync<TClass>(string url) where TClass : class
+        {
+            using (var client = new HttpClient())
+            {
+                try
+                {
+                    var response = await client.GetAsync(url).ConfigureAwait(false);
+                    var responseString = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+
+                    if (response.IsSuccessStatusCode)
+                        return JsonConvert.DeserializeObject<TClass>(responseString);
+
+                    Debug.WriteLine(responseString);
+                }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine("exception thrown: " + ex.Message);
+                }
+            }
+
+            return default(TClass);
+        }
+    }
+}
---
+++ QuoteCortanaSkill/QuoteCortanaSkill/Services/QuoteService.cs
@@ -0,0 +1,25 @@
+﻿using QuoteCortanaSkill.Models;
+using QuoteCortanaSkill.Utils;
+using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace QuoteCortanaSkill.Services
+{
+    public static class QuoteService
+    {
+        public static async Task<Quote> GetQuoteAsync()
+        {
+            try
+            {
+                var quote = await HttpService.GetRequestAsync<Quote>(Statics.QuoteUrl);
+                return quote;
+            }
+            catch(Exception ex)
+            {
+                Debug.WriteLine($"QuoteService | GetQuoteAsync | {ex}");
+                return null;
+            }
+        }
+    }
+}
