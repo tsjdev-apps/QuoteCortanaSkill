db6e01150c69b38561b80c9acd3ade5216e03e62
Sebastian Jensen <apps@tsjdev-apps.de>
2017-11-01 14:20:32 +0100

    - add model class

---
+++ QuoteCortanaSkill/QuoteCortanaSkill/Models/Quote.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+
+namespace QuoteCortanaSkill.Models
+{
+    public class Quote
+    {
+        [JsonProperty("quoteAuthor")]
+        public string QuoteAuthor { get; set; }
+
+        [JsonProperty("quoteLink")]
+        public string QuoteLink { get; set; }
+
+        [JsonProperty("quoteText")]
+        public string QuoteText { get; set; }
+
+        [JsonProperty("senderLink")]
+        public string SenderLink { get; set; }
+
+        [JsonProperty("senderName")]
+        public string SenderName { get; set; }
+    }
+}
